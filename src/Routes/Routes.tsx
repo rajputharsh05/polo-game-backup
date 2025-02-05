6c33e9f8d40cd0de0b03ccdeb23627c9dfa68046
rajputharsh05 <hsd020@nist.edu>
2025-02-05 23:16:14 +0530

    added build files and changes

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,10 +1,4 @@
-import { ReactNode } from "react";
 import Pages from "../Components/Pages";
-
-interface RouteBase {
-  path: string;
-  element: ReactNode;
-}
 
 export const globalRoutes = [
   {
